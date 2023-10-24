48bbb4a10f5391eafd5eafa0d9e719fdb680382a
Ziyu (Ashley) Ren <92663054+AshleyZR@users.noreply.github.com>
2023-10-24 01:54:11 +0000

    finished dice roll form practice

--- app.rb
+++ app.rb
@@ -2,8 +2,28 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/dice/:number_of_dice/:how_many_sides") do
+  @num_dice = params["number_of_dice"].to_i
+  @rolls = []
+  @sides = params["how_many_sides"].to_i
+
+  @num_dice.times do
+    die = rand(1..@sides)
+
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+end
+
+
+get ("/process_roll") do
+  number_of_dice = params["dice"].to_i
+  how_many_sides = params["sides"].to_i
+
+  # citation: redirect to function helped by chatgpt
+  redirect to("/dice/#{number_of_dice}/#{how_many_sides}")
 end
